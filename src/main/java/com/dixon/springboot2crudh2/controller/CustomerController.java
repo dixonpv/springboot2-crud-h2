b7003d433757ed4e348f90ffe692724b5221a69b
Dixon <dixonpv@gmail.com>
2020-10-18 14:49:19 -0400

    added get all customers

--- src/main/java/com/dixon/springboot2crudh2/controller/CustomerController.java
+++ src/main/java/com/dixon/springboot2crudh2/controller/CustomerController.java
@@ -4,9 +4,12 @@
  */
 package com.dixon.springboot2crudh2.controller;
 
-import javax.validation.Valid;	
+import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +29,15 @@ public class CustomerController {
 	@Autowired
 	private CustomerRepository customerRepository;
 	
+	@GetMapping("/customers")
+	public List<Customer> getAllCustomers() {
+		return customerRepository.findAll();
+	}
+	
 	@PostMapping("/customers")
 	public Customer createCustomer(@Valid @RequestBody Customer customer) {
 		return customerRepository.save(customer);
 	}
-
+	
+	
 }
